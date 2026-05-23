b7ccb9f6d925dd8a1cec5eda1d7ffa264917f15a
frostydbot <frostydbot@gmail.com>
2026-05-23 08:33:20 +0000

    Update default app logo to FrostyDBOT branding

--- src/components/layout/app-logo/index.tsx
+++ src/components/layout/app-logo/index.tsx
@@ -1,1 +1,7 @@
-export const AppLogo = () => null;
+export const AppLogo = () => (
+    <img
+        src='/images/frosty-logo.png'
+        alt='FrostyDBOT'
+        style={{ width: '2.4rem', height: '2.4rem', objectFit: 'contain' }}
+    />
+);
